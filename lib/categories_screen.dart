a3beabd76abf0c075f7daf68a8f4de38773546f2
Wojcikowski Frederic <wojcikowskif@gmail.com>
2021-03-06 22:41:40 +0900

    register the home page

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -6,16 +6,21 @@ import './dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        mainAxisSpacing: 20,
-        crossAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
       ),
-      children: DUMMY_CATEGORIES
-          .map((category) => CategoryItem(category.title, category.color))
-          .toList(),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((category) => CategoryItem(category.title, category.color))
+            .toList(),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: CategoriesScreen(),
+      home: CategoriesScreen(),
     );
   }
 }
